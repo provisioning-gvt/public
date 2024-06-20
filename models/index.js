a4c7b8b3dcd20b70d0a69a41aecfb2cdcdeefd44
Khi Ann Chua <chua_khi_ann@tech.gov.sg>
2024-06-20 09:45:47 +0800

    feat: added debug

--- models/index.js
+++ models/index.js
@@ -1,16 +1,19 @@
 const dbConfig = require("../db.config.js");
 const Sequelize = require("sequelize");
-
+const db = {};
+try {
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
   dialectOptions: dbConfig.dialectOptions,
 });
 
-const db = {};
+
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 db.products = require("./product.model.js")(sequelize, Sequelize);
-
+} catch (error) {
+  console.error("An error occurred during setup: ", error);
+}
 module.exports = db;
