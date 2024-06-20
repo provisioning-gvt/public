a294323eecee649f1913373cb0c14c8c10ee972f
Khi Ann Chua <chua_khi_ann@tech.gov.sg>
2024-06-20 09:57:52 +0800

    debug: to rule out problem

--- db.config.js
+++ db.config.js
@@ -1,9 +1,24 @@
 require('dotenv').config()
-
+/*
 module.exports = {
   HOST: "gcc-canary-sqlserver.database.windows.net",
   USER: process.env.DB_USER,
   PASSWORD: process.env.DB_PASSWORD,
+  
+  DB: "gcc-canary-sqldatabase",
+  dialect: "mssql",
+  dialectOptions: {
+    options: {
+      encrypt: true,
+    },
+  },
+};
+*/
+
+module.exports = {
+  HOST: "gcc-canary-sqlserver.database.windows.net",
+  USER: "adminuser",
+  PASSWORD: "Ohmygodthisisbad123",
   
   DB: "gcc-canary-sqldatabase",
   dialect: "mssql",
